08aa608905aa5f394e42caa1e7f258704a92ad30
AnaHertaj <ana@MacBook-Pro-de-Ana.local>
2019-01-22 13:36:09 +0100

    desglose
    desglose

---
+++ video_AnaHertaj.js
@@ -0,0 +1,69 @@
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '360',
+          width: '640',
+          videoId: 'ISJMsVhY5Yk',
+          //videoID: '6DAPmSbZoYY',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }  
+
+
+      }
+     
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+
+$( document ).ready(function(){
+
+  $('#playpause').toggle(
+
+    
+          function(e){
+            
+                 player.playVideo();
+                 $('#playpause').html('PAUSE');
+                 e.preventDefault();
+          
+          },
+
+          function(e){
+               
+                   player.pauseVideo();
+                   $('#playpause').html('PLAY');
+                   e.preventDefault();
+               
+          }
+
+        );
+ });
